352e2c6b1a7c1c3e544fd0a3fda62c91e9df2cff
leekwanreal <ngleekwan113@gmail.com>
2026-08-30 17:35:19 +0700

    Remove token and add extracted paper text

--- lookahead_sampling.py
+++ lookahead_sampling.py
@@ -130,7 +130,7 @@ def main(args):
         base_model_id = "black-forest-labs/FLUX.1-dev"
         repo_name = "ByteDance/Hyper-SD"
         ckpt_name = "Hyper-FLUX.1-dev-8steps-lora.safetensors"
-        pipe = FluxPipeline.from_pretrained(base_model_id, token="")
+        pipe = FluxPipeline.from_pretrained(base_model_id, token=os.environ.get("HF_TOKEN", True))
         pipe.load_lora_weights(hf_hub_download(repo_name, ckpt_name))
         pipe.fuse_lora(lora_scale=0.125)
         pipe.to("cuda", dtype=torch.float16)
